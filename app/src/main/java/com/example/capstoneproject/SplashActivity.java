7d95f28bbf84a262a4bc27c5bc71410cf7dc0602
ErickH <ehhernandezh@gmail.com>
2020-10-04 13:54:23 -0400

    Erick: Added Splash Screen

--- app/src/main/java/com/example/capstoneproject/SplashActivity.java
+++ app/src/main/java/com/example/capstoneproject/SplashActivity.java
@@ -1,28 +1,48 @@
 package com.example.capstoneproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SplashActivity extends AppCompatActivity {
+    private  static int SPLASH_SCREEN= 4500;
+
+    Animation topAnim, bottomAnim;
+    View image;
+    TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-    }
 
-    //Erick H
-    //hiiiiiiii
-    // Natalie
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
 
-    public void erickMethod(){
-        //test
-        //test
-    }
+        image = findViewById(R.id.logo);
+        text = findViewById(R.id.logoText);
 
-    public void natalieMethod(){
-        // testing
+        image.setAnimation(topAnim);
+        text.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
 
