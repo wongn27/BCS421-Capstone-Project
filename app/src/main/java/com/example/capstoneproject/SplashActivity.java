121eaf19d94b72c08871f3c5410619d245b0d5ed
ErickH <ehhernandezh@gmail.com>
2020-10-04 12:39:35 -0400

    Erick: Renamed MainActivity to SplashActivity

--- app/src/main/java/com/example/capstoneproject/SplashActivity.java
+++ app/src/main/java/com/example/capstoneproject/SplashActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 
     //Erick H
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void natalieMethod(){
         // testing
-
     }
 }
 
