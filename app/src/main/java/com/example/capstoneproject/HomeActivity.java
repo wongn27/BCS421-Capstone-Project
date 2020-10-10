855cb032f34ecc729259abc24ac130b2cbf67749
ErickH <ehhernandezh@gmail.com>
2020-10-10 16:30:59 -0400

    Erick: Added RegistrationActivity and LoginActivity

---
+++ app/src/main/java/com/example/capstoneproject/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.capstoneproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnLogout = findViewById(R.id.logout);
+
+        btnLogout.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToLogin = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intToLogin);
+            }
+        }));
+    }
+}
