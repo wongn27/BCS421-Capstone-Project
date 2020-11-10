8b13c8ed7f4158173a88a65aef6341c7bdd51ae1
Will Leuenroth <leuenroo@gmail.com>
2020-11-10 11:06:01 -0500

    hi -Will

--- app/src/main/java/com/example/capstoneproject/EditPetProfileActivity.java
+++ app/src/main/java/com/example/capstoneproject/EditPetProfileActivity.java
@@ -10,5 +10,6 @@ public class EditPetProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_pet_profile);
+
     }
 }
