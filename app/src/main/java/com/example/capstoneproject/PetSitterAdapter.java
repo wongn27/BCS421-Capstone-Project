5f234cef61783fa1a2a63a974dd34cb79279d1e8
ErickH <ehhernandezh@gmail.com>
2020-12-14 12:34:28 -0500

    Erick: Filters by isPetSitter

--- app/src/main/java/com/example/capstoneproject/PetSitterAdapter.java
+++ app/src/main/java/com/example/capstoneproject/PetSitterAdapter.java
@@ -8,6 +8,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
@@ -15,29 +17,26 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class PetSitterAdapter extends FirestoreRecyclerAdapter<PetSitter, PetSitterAdapter.PetSitterHolder> {
-    private OnItemClickListener listener;
-    private Context context;
+public class PetSitterAdapter extends RecyclerView.Adapter<PetSitterAdapter.PetSitterHolder> {
+    //private OnItemClickListener listener;
+    //private Context context;
+    List<PetSitter> petSitterList;
+    List<CardView> cardViewList;
+    OnPetSitterListener onPetSitterListener;
+    //LocalBroadcastManager localBroadcastManager;
 
-    public PetSitterAdapter(@NonNull FirestoreRecyclerOptions<PetSitter> options) {
-        super(options);
-    }
 
-    public PetSitterAdapter(@NonNull FirestoreRecyclerOptions<PetSitter> options, Context context) {
-        super(options);
-        this.context = context;
-    }
-
-    @Override
-    protected void onBindViewHolder(@NonNull PetSitterHolder holder, int position, @NonNull PetSitter model) {
-        holder.textViewName.setText(model.getfName() + " " + model.getlName());
-        //holder.textViewMiles.setText(model.getEmail());
-        //holder.textViewRating.setText(model.getlName());
-
-        Picasso.with(context).load(model.getImageUrl()).into(holder.imageView);
-        //holder.imageView.setImageResource(model.);
+    public PetSitterAdapter(List<PetSitter> petSitterList, OnPetSitterListener onPetSitterListener) {
+        //this.context = context;
+        this.petSitterList = petSitterList;
+        cardViewList = new ArrayList<>();
+        this.onPetSitterListener = onPetSitterListener;
+        //localBroadcastManager = LocalBroadcastManager.getInstance(context);
     }
 
     @NonNull
@@ -45,45 +44,68 @@ public class PetSitterAdapter extends FirestoreRecyclerAdapter<PetSitter, PetSit
     public PetSitterHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.sitter_item,
                 parent, false);
-        context = parent.getContext();
-        return new PetSitterHolder(v);
+        //context = parent.getContext();
+        return new PetSitterHolder(v, onPetSitterListener);
     }
 
-    class PetSitterHolder extends RecyclerView.ViewHolder{
+    @Override
+    public void onBindViewHolder(@NonNull final PetSitterHolder holder, int i) {
+        holder.textViewName.setText(petSitterList.get(i).getfName() + " " + petSitterList.get(i).getlName());
+        //holder.textViewMiles.setText(model.getEmail());
+        //holder.textViewRating.setText(model.getlName());
+
+        if(!cardViewList.contains((holder.cardView))){
+            cardViewList.add(holder.cardView);
+        }
+
+        //Picasso.with(context).load(model.getImageUrl()).into(holder.imageView);
+        //holder.imageView.setImageResource(model.);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return petSitterList.size();
+    }
+
+
+    public class PetSitterHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView textViewName;
         //TextView textViewRating;
         //TextView textViewMiles;
         CircleImageView imageView;
+        CardView cardView;
+        OnPetSitterListener onPetSitterListener;
 
-
-        public PetSitterHolder(View itemView) {
+        public PetSitterHolder(View itemView, OnPetSitterListener onPetSitterListener) {
             super(itemView);
+            cardView = itemView.findViewById(R.id.cardView);
             textViewName = itemView.findViewById(R.id.fullName);
             //textViewRating = itemView.findViewById(R.id.rating);
             //textViewMiles = itemView.findViewById(R.id.milesAway);
             imageView = itemView.findViewById(R.id.profilePic);
+            this.onPetSitterListener = onPetSitterListener;
 
+            itemView.setOnClickListener(this);
 
-            itemView.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    int position = getAdapterPosition();
-                    //position is valid
-                    if (position != RecyclerView.NO_POSITION && listener != null){
-                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
-                    }
-
-                }
-            });
         }
 
+        @Override
+        public void onClick(View v) {
+            onPetSitterListener.onPetSitterClick(getAdapterPosition());
+        }
     }
 
-    //send data from the adapter to the activity
+    public interface OnPetSitterListener{
+        void onPetSitterClick(int position);
+    }
+
+
+    /*//send data from the adapter to the activity
     public interface OnItemClickListener {
         void onItemClick(DocumentSnapshot documentSnapshot, int position);
     }
     public void setOnItemClickListener(OnItemClickListener listener){
         this.listener = listener;
-    }
+    }*/
 }
